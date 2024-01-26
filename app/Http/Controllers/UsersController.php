90ede073e99ffb2d45bdc6f633fcaa272656d443
YWJ-ID <70832062+YWJ-ID@users.noreply.github.com>
2024-01-26 17:22:39 +0800

    完成用户注册

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,6 +20,7 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
+        //    保存用户并且重定向
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -27,6 +28,19 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        $data = $request->all();
+
+//        用户模型 User::create() 创建成功后会返回一个用户对象，并包含新注册用户的所有信息。我们将新注册用户的所有信息赋值给变量 $user，并通过路由跳转来进行数据绑定。
+        $user = User::created([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+            'password' => bcrypt($request->password),
+        ]);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+//     『约定优于配置』的体现，此时 $user 是 User 模型对象的实例。route() 方法会自动获取 Model 的主键，也就是数据表 users 的主键 id，以上代码等同于：
+//      redirect()->route('users.show', [$user->id]);
+        return redirect()->route('users.show', [$user]);
     }
 }
